a49c164baf92b6cdd8c1efbd87d58a7e2eb52cde
oleksii.oleksii <oleksii.oleksii@whitebit.com>
2022-05-20 12:52:43 +0300

    feat(Pair): add coming soon to pair chart

--- src/components/ComingSoon/styled.ts
+++ src/components/ComingSoon/styled.ts
@@ -7,6 +7,9 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  border-radius: 0.75rem;
+  background-color: #47192b29;
+  overflow: hidden;
 `
 
 export const Chart = styled.img`
@@ -14,19 +17,15 @@ export const Chart = styled.img`
   height: 100%;
   filter: blur(6px);
   border-radius: 0.6rem;
+  transform: scale(1.5);
 `
 export const Title = styled.span`
-  width: 100%;
-  height: 100%;
   display: inline-flex;
   align-items: center;
   justify-content: center;
   font-size: 2.5rem;
   position: absolute;
-  padding: 0.5rem;
-  border-radius: 12px;
   color: ${({ theme }) => theme.white};
-  background: #570a242b;
 
   @media screen and (max-width: 600px) {
     font-size: 1.25rem;
