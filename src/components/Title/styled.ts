f08fd06e1ccff31b8a27f20c1c9eeb151240b795
oleksii.oleksii <oleksii.oleksii@whitebit.com>
2023-05-08 11:26:44 +0200

    style: change logo

--- src/components/Title/styled.ts
+++ src/components/Title/styled.ts
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const TitleWrapper = styled(Link)`
-  text-decoration: none;
   cursor: pointer;
-  height: 2rem;
 
   @media screen and (min-width: 1080px) {
     margin: 1.5rem 2rem;
+  }
+
+  img {
+    max-width: 100%;
   }
 `
