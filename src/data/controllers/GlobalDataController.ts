4355b215c274608f5f7fb52a0760cec29e26b51a
oleksii.oleksii <oleksii.oleksii@whitebit.com>
2023-05-10 10:26:41 +0200

    fix(Eth): handle empty block for eth

--- src/data/controllers/GlobalDataController.ts
+++ src/data/controllers/GlobalDataController.ts
@@ -1,15 +1,23 @@
 import { IGlobalDataController } from 'data/controllers/types/GlobalController.interface'
 import { client } from 'service/client'
-import { CurrentTrxPriceQuery, GlobalChartQuery, GlobalChartQueryVariables } from 'service/generated/graphql'
-import { CURRENT_TRX_PRICE, GLOBAL_CHART } from 'service/queries/global'
+import {
+  CurrentTrxPriceQuery,
+  GlobalChartQuery,
+  GlobalChartQueryVariables,
+  LastBlockQuery
+} from 'service/generated/graphql'
+import { CURRENT_TRX_PRICE, GLOBAL_CHART, LAST_BLOCK } from 'service/queries/global'
 
 export default class GlobalDataController implements IGlobalDataController {
   async getHealthStatus() {
-    // FIXME: load data from server. Temporary solution for ETH
-    // const { data } = await client.query<LastBlockQuery>({ query: LAST_BLOCK })
-    const { data } = await Promise.resolve({ data: { lastBlock: 17_223_893 } })
-    // lastBlock === headBlock because TRONWEB cannot return headBlock
-    return { syncedBlock: data.lastBlock, headBlock: data.lastBlock }
+    try {
+      const { data } = await client.query<LastBlockQuery>({ query: LAST_BLOCK })
+      // lastBlock === headBlock because TRONWEB cannot return headBlock
+      return { syncedBlock: data.lastBlock, headBlock: data.lastBlock }
+    } catch {
+      // FIXME: load data from server. Temporary solution for ETH
+      return { syncedBlock: 0.1, headBlock: 0.1 }
+    }
   }
   async getChartData(oldestDateToFetch: number): Promise<ChartDailyItem[]> {
     const { data } = await client.query<GlobalChartQuery, GlobalChartQueryVariables>({
