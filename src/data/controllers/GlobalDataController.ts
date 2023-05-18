8914106c9285927301c156ee201fe3ff4108161b
oleksii.oleksii <oleksii.oleksii@whitebit.com>
2023-05-18 12:19:13 +0200

    feat(Eth): load eth current price & last block

--- src/data/controllers/GlobalDataController.ts
+++ src/data/controllers/GlobalDataController.ts
@@ -1,23 +1,18 @@
 import { IGlobalDataController } from 'data/controllers/types/GlobalController.interface'
 import { client } from 'service/client'
 import {
-  CurrentTrxPriceQuery,
   GlobalChartQuery,
   GlobalChartQueryVariables,
-  LastBlockQuery
+  LastBlockQuery,
+  NativeTokenPriceQuery
 } from 'service/generated/graphql'
-import { CURRENT_TRX_PRICE, GLOBAL_CHART, LAST_BLOCK } from 'service/queries/global'
+import { NATIVE_TOKEN_PRICE, GLOBAL_CHART, LAST_BLOCK } from 'service/queries/global'
 
 export default class GlobalDataController implements IGlobalDataController {
   async getHealthStatus() {
-    try {
-      const { data } = await client.query<LastBlockQuery>({ query: LAST_BLOCK })
-      // lastBlock === headBlock because TRONWEB cannot return headBlock
-      return { syncedBlock: data.lastBlock, headBlock: data.lastBlock }
-    } catch {
-      // FIXME: load data from server. Temporary solution for ETH
-      return { syncedBlock: 0.1, headBlock: 0.1 }
-    }
+    const { data } = await client.query<LastBlockQuery>({ query: LAST_BLOCK })
+    // lastBlock === headBlock because TRONWEB cannot return headBlock
+    return { syncedBlock: data.lastBlock, headBlock: data.lastBlock }
   }
   async getChartData(oldestDateToFetch: number): Promise<ChartDailyItem[]> {
     const { data } = await client.query<GlobalChartQuery, GlobalChartQueryVariables>({
@@ -33,7 +28,7 @@ export default class GlobalDataController implements IGlobalDataController {
   }
 
   async getPrice() {
-    const { data } = await client.query<CurrentTrxPriceQuery>({ query: CURRENT_TRX_PRICE })
-    return data.trxPrice ? +data.trxPrice : 0
+    const { data } = await client.query<NativeTokenPriceQuery>({ query: NATIVE_TOKEN_PRICE })
+    return data.nativeTokenPrice ? +data.nativeTokenPrice : 0
   }
 }
