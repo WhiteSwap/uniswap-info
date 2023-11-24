70e6e4f0fde3d1e93248f197389c8d7ec1900483
Kayvin <kayvin4@hotmail.com>
2023-11-24 16:21:35 +0100

    fix: add vite config + html entry point

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,29 @@
-import react from '@vitejs/plugin-react'
+import inject from '@rollup/plugin-inject'
+import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import { checker } from 'vite-plugin-checker'
 import svgrPlugin from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(() => {
   return {
-    build: {
-      outDir: 'build'
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      svgrPlugin(),
+      checker({ typescript: true }),
+      inject({
+        Buffer: ['buffer', 'Buffer']
+      })
+    ],
+    server: {
+      hmr: { overlay: false },
+      host: '0.0.0.0',
+      port: 3000
     },
-    plugins: [react(), tsconfigPaths(), svgrPlugin({ svgrOptions: { icon: true } })],
-    esbuild: {
-      loader: 'tsx'
+    define: {
+      global: {},
+      Buffer: ['buffer', 'Buffer']
     }
   }
 })
