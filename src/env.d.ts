b5ee672e5b9dcf3c12d9c1e36e66779e3f23b67a
Natalia Gorbova <natalia.gorbova@ec319.com>
2023-12-13 12:08:22 +0100

    feat: update types

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,11 +2,16 @@
 
 declare namespace NodeJS {
   interface ImportMetaEnvironment {
-    readonly VITE_TITLE: string
+    VITE_ENV: string
+    VITE_VERSION: string
     VITE_TRON_GRAPH: string
     VITE_ETHEREUM_GRAPH: string
     VITE_POLYGON_GRAPH: string
+
     VITE_GOOGLE_ANALYTICS_ID: string
+    VITE_GTM_ID: string
+
+    VITE_SENTRY_DSN: string
   }
 }
 
